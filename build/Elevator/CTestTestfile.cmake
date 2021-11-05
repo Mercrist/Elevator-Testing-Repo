9b04e24c5f88fbc764a19642df476c5dc01376d5
Mercrist <yariel.then@gmail.com>
2021-11-05 16:21:26 -0400

    Cleaned build folder

--- build/Elevator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/yarie/Desktop/Elevator Testing Repo/Elevator
-# Build directory: C:/Users/yarie/Desktop/Elevator Testing Repo/build/Elevator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/States/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/yarie/Desktop/Elevator Testing Repo/States
-# Build directory: C:/Users/yarie/Desktop/Elevator Testing Repo/build/States
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
